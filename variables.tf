f628ef5fe01f0df650ea240d8490aa5b09109eb3
ntwauto <hammani.b@gmail.com>
2025-11-05 12:28:04 -0500

    Refactor variables in variables.tf
    Removed descriptions from existing variables and added a new variable for environment.

--- variables.tf
+++ variables.tf
@@ -1,15 +1,4 @@
-variable "subgroup" {
-  description = "Subgroup (e.g., neteng or wineng)"
-  type        = string
-}
-
-variable "project_name" {
-  description = "Project name (used for container)"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "eastus"
-}
+variable "subgroup" {}
+variable "project_name" {}
+variable "location" { default = "eastus" }
+variable "environment" {}
