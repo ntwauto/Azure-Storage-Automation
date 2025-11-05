63e59201ebf8e77be9bdcf718eeac57f905b0be6
ntwauto <hammani.b@gmail.com>
2025-11-05 12:33:02 -0500

    Add ARM authentication variables to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
 variable "subgroup" {}
 variable "project_name" {}
-variable "location" { default = "eastus" }
 variable "environment" {}
+variable "location" { default = "eastus" }
+
+variable "arm_client_id" {}
+variable "arm_client_secret" {}
+variable "arm_tenant_id" {}
+variable "arm_subscription_id" {}
