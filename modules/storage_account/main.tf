8fb73d8aff1f9dffd2f82f0394a30526e31ca936
ntwauto <hammani.b@gmail.com>
2025-11-05 12:40:46 -0500

    Refactor storage account and resource group naming
    Updated storage account resource to include a random suffix and modified resource group naming convention.

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -1,18 +1,35 @@
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "${var.subgroup}-${var.environment}-rg"
   location = var.location
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
+  # ✅ Fixed: prefixed with "st", includes subgroup/env, and adds random suffix
+  name                     = lower("st${var.subgroup}${var.environment}${random_string.suffix.result}")
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  allow_blob_public_access = false
 }
 
 resource "azurerm_storage_container" "container" {
   name                  = var.container_name
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.container.name
 }
