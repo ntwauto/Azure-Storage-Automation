c3d2020e5c3706af9565f0adbe6b727c8240412b
ntwauto <hammani.b@gmail.com>
2025-11-05 12:25:31 -0500

    Simplify variable definitions in variables.tf
    Removed descriptions from storage account variables.

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -1,27 +1,4 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Resource group name"
-}
-
-variable "storage_account_name" {
-  type        = string
-  description = "Storage account name"
-}
-
-variable "container_name" {
-  type        = string
-  description = "Container name for the project"
-}
-
-variable "location" {
-  type        = string
-  default     = "eastus"
-}
-
-variable "subgroup" {
-  type        = string
-}
-
-variable "environment" {
-  type        = string
-}
+variable "resource_group_name" {}
+variable "storage_account_name" {}
+variable "container_name" {}
+variable "location" { default = "eastus" }
